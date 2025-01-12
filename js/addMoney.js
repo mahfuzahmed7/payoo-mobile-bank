98629544c3ea3c726d766f5db87b655c971b4f2b
Mahfuz Ahmed <108610927+mahfuzahmed7@users.noreply.github.com>
2025-01-12 20:33:50 +0600

    added add money function

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,6 +1,16 @@
 document.getElementById('btn-add-money').addEventListener('click', function (event) {
     event.preventDefault();
 
+
     // const addMoney = document.getElementById('input-add-money').value;
     // const addMoneyNumber = parseFloat(addMoney);
+
+    // getInputFieldValueById();
+    // const addMoney = getInputFieldValueById();
+
+    const addMoney = getInputFieldValueById('input-add-money');
+    const pinNumber = getInputFieldValueById('input-pin-number');
+    console.log('add money with parameter', addMoney, pinNumber);
+
+
 });
--- js/utilities.js
+++ js/utilities.js
@@ -2,6 +2,13 @@
  * common shared function here
  */
 
-function getInputFieldValueById() {
-    console.log('will ge vlaue by id');
+// function getInputFieldValueById() {
+//     console.log('will get value by id')
+//     const addMoney = document.getElementById('input-add-money').value;
+//     return addMoney;
+// }
+
+function getInputFieldValueById(id) {
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
 }
